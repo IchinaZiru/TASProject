4ccf55ffa5b3df81c925fd8995cd64505e09374d
IchinaZiru <yutaihuangjing08@gmail.com>
2024-10-12 09:32:10 +0900

    Delete STFP.bat

--- STFP/STFP.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start "%cd%" "%cd%\left4dead2.exe" -lv -novid -insecure +plugin_load cfg\sm_server\metamod\bin\server.dll +mm_basedir cfg\sm_server\metamod +sm_basepath cfg\sm_server\sourcemod
-exit
